5d5ef71a8f7c6fa0df92ddcd1ce5f8470b2f2431
yuanyuan <yuanyuan@beisen.com>
2016-03-01 10:48:37 +0800

    first commit\

---
+++ index.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+import React, {Component, PropTypes} from 'react'
+import {render} from 'react-dom'
+
+
+class App extends Component {
+	render () {
+		return (
+			<h1>11111</h1>
+		)
+	}
+}
+render(
+	<h1>hello world</h1>, 
+document.getElementById('content'))
