fae8040554be221a5d48f58709d2ddd5f827a3f2
李苏晗 <lisuhan@MacBook.local>
2017-08-02 10:14:18 +0800

    add:webpack完善

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,47 @@
 var path = require("path");
+
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //自动引用了你打包后的JS文件
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
 	entry : './src/entry.js',
 	module : {
 		loaders : [
 			{
-				loader : 'babel-loader',
 				test : /\.js$/,
+				loader : 'babel-loader',
 				exclude : /node_modules/
 			}
 			, {
-		      test: /\.(css|scss)$/,
-		      loaders: [ 'style-loader', 'css-loader', 'sass-loader'],
-		      include: __dirname,
-		      exclude : /node_modules/
+		        test: /\.(css|scss)$/,
+		        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+		        exclude : /node_modules/
 		    }
 		]
 	},
 	output: {
 		filename: "[name].bundle.js",
-		path: path.join(__dirname, "/dist"),
+		path: __dirname + "/dist",
 		publicPath:'/dist',
 		chunkFilename: '[name].[chunkhash:5].chunk.js',
 	},
 	devServer : {
 		publicPath : '/dist',
 		filename : 'main.bundle.js',
+		contentBase: "./dist",
 		host : '0.0.0.0',
-		port : 8080
+		port : 8080,
+		historyApiFallback: true,//不跳转
 	},
 	plugins: [
-	// //必须配置，react的公共模块
+	//     必须配置，react的公共模块
 	//     new webpack.optimize.CommonsChunkPlugin({
 	//       names: ['vendor'],
 	//       filename: 'vendor.js'
 	//     })
+		 new HtmlWebpackPlugin({
+	      	template: __dirname + "/index.html"
+	    }),
 	],
+	devtool: 'eval-source-map',
 }
