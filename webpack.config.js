229fcee24c3f4d8d2ed406cfc71cc593e6c6ee81
lisuhan <496418726@qq.com>
2018-01-03 19:04:43 +0800

    增加dll插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,104 +1,117 @@
-var path = require("path");
-var webpack = require('webpack');
+const path = require("path")
+const webpack = require("webpack")
 
-var HtmlWebpackPlugin = require('html-webpack-plugin'); //自动引用打包后的JS文件
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var buildProd = process.env.NODE_ENV === "production"; //是否要上线环境
+const HtmlWebpackPlugin = require("html-webpack-plugin") //自动引用打包后的JS文件
+const ExtractTextPlugin = require("extract-text-webpack-plugin") //分离css为单独文件
+const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin") //往html中添加dll文件
+const CleanWebpackPlugin = require("clean-webpack-plugin") //删除dist下的缓存
 
+const buildProd = process.env.NODE_ENV === "production" //是否要上线环境
 
 /*plugins list*/
 const plugins = [
-	new webpack.optimize.CommonsChunkPlugin({  //提取公共模块
-        name: "vendor",
-        minChunks: Infinity
+    new HtmlWebpackPlugin({
+        //插入模板中
+        filename: "index.html",
+        template: path.resolve(__dirname + "/index.html"),
+        inject: "body",
     }),
-	new HtmlWebpackPlugin({						//插入模板中
-		filename:'index.html',
-		template: __dirname + "/index.html",
-		inject:'body'
+    new ExtractTextPlugin("css/[name].css", {
+        disable: false,
+        allChunks: true,
+    }), //分离css
+    new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: require("./manifest.json"),
     }),
-	new ExtractTextPlugin("css/[name].css",{disable: false,allChunks: true}),//分离css
-];
-if(buildProd){
-	plugins.push(
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-			    warnings: false,
-			    screw_ie8: false
-			},
-			mangle: { screw_ie8: false },
-			output: { screw_ie8: false },
-			sourceMap: true
-		})
-	)
-}else{
-	plugins.push(
-		new webpack.HotModuleReplacementPlugin()
-	)
+    new AddAssetHtmlPlugin({
+        filepath: require.resolve(__dirname + "/dll/dll.js"),
+        includeSourcemap: false,
+    }),
+    new CleanWebpackPlugin("dist/*.*", {
+        root: __dirname,
+        verbose: true,
+        dry: false,
+    }),
+]
+if (buildProd) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                screw_ie8: false,
+            },
+            mangle: { screw_ie8: false },
+            output: { screw_ie8: false },
+            sourceMap: true,
+        })
+    )
+} else {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
-
-
 module.exports = {
-	entry : {
-		vendor:['react','redux','react-redux','react-dom','immutable'],
-		app:'./src/entry.js'
-	},
-	module : {
-		rules : [
-			{
-				test : /\.js$/,
-				use : 'babel-loader',
-				exclude : /node_modules/
-			}
-			,{
-		        test: /\.css$/,
-		        use: ExtractTextPlugin.extract({
-		        	fallback:'style-loader',
-					use:['css-loader?sourceMap','postcss-loader']
-		        }),
-		        exclude : /node_modules/
-		    }
-		    ,{
-		    	test: /\.scss$/,
-		    	use: ExtractTextPlugin.extract({
-		        	fallback:'style-loader',
-					use:['css-loader?sourceMap','postcss-loader', 'sass-loader'],
-		        }),
-		    	exclude : /node_modules/
-		    }
-		    ,{	
-             	test: /\.(png|jpg|gif)$/,   					// 图片加载器，同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求  													
-             	use: 'url-loader?limit=8192&name=./images/[hash:5].[name].[ext]',// 将小于8192byte的图片转成base64码
-            }
-            ,{
-	            test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
-	            use: 'file-loader?name=./fonts/[hash:5].[name].[ext]',
-	        },
-		]
-	},
-	resolve:{
-		alias:{
-			'&':path.resolve(__dirname, "src")
-		}
-	},
-	output: {
-		filename: buildProd
-			? '[name].bundle.min.js'
-			: '[name].bundle.js',
-		chunkFilename: buildProd
-			? '[name].[chunkhash:5].chunk.min.js'
-			: '[name].[chunkhash:5].chunk.js',
-		path: path.resolve(__dirname,"dist/"),
-	},
-	devServer : {
-		publicPath : '/',
-	//	contentBase:path.resolve(__dirname, "dist/"),
-		host : '127.0.0.1',
-		port : 8000,
-		hot : true,
-		historyApiFallback: true,//不跳转
-	},
-	plugins: plugins,
-	devtool: buildProd ? false : 'eval-source-map',
+    entry: {
+        app: "./src/entry.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader?sourceMap", "postcss-loader"],
+                }),
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        "css-loader?sourceMap",
+                        "postcss-loader",
+                        "sass-loader",
+                    ],
+                }),
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpg|gif)$/, // 图片加载器，同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
+                use:
+                    "url-loader?limit=8192&name=./images/[hash:5].[name].[ext]", // 将小于8192byte的图片转成base64码
+            },
+            {
+                test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
+                use: "file-loader?name=./fonts/[hash:5].[name].[ext]",
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            "&": path.resolve(__dirname, "src"),
+        },
+    },
+    output: {
+        filename: buildProd ? "[name].bundle.min.js" : "[name].bundle.js",
+        chunkFilename: buildProd
+            ? "[name].[chunkhash:5].chunk.min.js"
+            : "[name].[chunkhash:5].chunk.js",
+        path: path.resolve(__dirname, "dist/"),
+    },
+    devServer: {
+        publicPath: "/",
+        //	contentBase:path.resolve(__dirname, "dist/"),
+        host: "127.0.0.1",
+        port: 8000,
+        hot: true,
+        historyApiFallback: true, //不跳转
+    },
+    plugins: plugins,
+    devtool: buildProd ? false : "eval-source-map",
 }
---
+++ webpack.dll.config.js
@@ -0,0 +1,28 @@
+const webpack = require("webpack")
+const path = require("path")
+
+const vendors = [
+    "react",
+    "react-dom",
+    "react-router",
+    "react-router-dom",
+    "redux",
+]
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, "dll/"),
+        filename: "[name].js",
+        library: "[name]",
+    },
+    entry: {
+        dll: vendors,
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: "manifest.json",
+            name: "[name]",
+            context: __dirname,
+        }),
+    ],
+}
